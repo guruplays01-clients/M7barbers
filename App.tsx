4187c1830261f0a5d7c7cb492b2b7c6c2d259b8c
guruplays01-clients <guruplays01@gmail.com>
2025-12-28 13:51:19 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import CustomCursor from './components/CustomCursor';
